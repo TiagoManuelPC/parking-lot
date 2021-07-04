eae2d1e7d39a219f6727af7e30366b04f0d8cb54
TiagoManuelPC <tiagomanuelpereiracampos@hotmail.com>
2021-07-04 17:27:10 +0100

    bus park has a capacity of 5

--- lib/bus_lot.rb
+++ lib/bus_lot.rb
@@ -1,9 +1,11 @@
 class Bus_Lot
 
-attr_reader :spaces
+attr_reader :spaces, :capacity
+::MaximunBusCapacity = 5
 
 def initialize
   @spaces = []
+  @capacity = MaximunBusCapacity
 end
 
 end
--- spec/bus_lot_spec.rb
+++ spec/bus_lot_spec.rb
@@ -3,8 +3,11 @@ require 'bus_lot'
 describe Bus_Lot do
 subject(:bus_lot) {Bus_Lot.new}
 
-it 'has spaces for cars' do
+it 'has spaces for bus' do
 expect(bus_lot.spaces).to eq []
+end
+it 'has a maximun capacity of 5' do
+  expect(bus_lot.capacity).to eq 5
 end
 
 end
